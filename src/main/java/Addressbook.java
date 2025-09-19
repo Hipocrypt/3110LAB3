0a2bbb1f08388c57fb1d5c83137db4b34dc3dfcc
Daniel Esenwa <dansnwa@@gmail.com>
2025-09-19 15:32:57 -0400

    new addressbook

---
+++ src/main/java/Addressbook.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Addressbook {
+
+    ArrayList<Buddyinfo> myList = new ArrayList<>();
+    public void addBuddy(Buddyinfo buddyinfo) {
+        myList.add(buddyinfo);
+    }
+
+    public void removeBuddy(Buddyinfo buddyinfo) {
+        myList.remove(buddyinfo);
+    }
+
+    public static void main(String[] args) {
+        Buddyinfo buddy = new  Buddyinfo("Daniel Esenwa", "Carleton", "99");
+        Addressbook addressBook = new Addressbook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
