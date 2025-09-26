ad66d982724e4772bdc91c70d1c5acb08f7fd897
Daniel Esenwa <dansnwa@@gmail.com>
2025-09-26 15:27:41 -0400

    Added new method clearbuddy()

--- src/main/java/Addressbook.java
+++ src/main/java/Addressbook.java
@@ -9,11 +9,14 @@ public class Addressbook {
     public void removeBuddy(Buddyinfo buddyinfo) {
         myList.remove(buddyinfo);
     }
-
+    public void clearBuddy() {
+        myList.clear();
+    }
     public static void main(String[] args) {
         Buddyinfo buddy = new  Buddyinfo("Samuel", "Cambridge", "100");
         Addressbook addressBook = new Addressbook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        addressBook.clearBuddy();
     }
 }
